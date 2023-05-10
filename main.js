d56952810badacecfb83ce6206e5f4cdc8bd8f29
yildirimali0 <yildirimaliyasir97@gmail.com>
2023-05-10 11:50:18 +0300

    portfolio

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import './src/tailwind.css'
 
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove",moveCursor)
+
+function moveCursor(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    innerCursor.style.left = '${x}px';
+    innerCursor.style.top = '${y}px';
+    outerCursor.style.left = '${x}px';
+    outerCursor.style.top = '${y}px';
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        '1040': '1040px',
+      },
+      height: {
+        'header': '63.5px',
+      },
+      colors:{
+        "hed": "#0b0e13",
+        "link": "#eee",
+        "navbg": "#222222",
+        "mainbg": "#333",
+        "pr":"#CCCCCC"
+      }
+    },
+    fontFamily:{
+      montserrat:["Montserrat","sans-serif"],
+    },
     container:{
       center:true,
     },
